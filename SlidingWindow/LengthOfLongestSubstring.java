eb794cece4cd8b9555a4b5f517236fdc35051684
Deepika Teegapuram <dteegapuram@gmail.com>
2024-07-24 10:27:03 -0600

    length of unique subarray

--- SlidingWindow/LengthOfLongestSubstring.java
+++ SlidingWindow/LengthOfLongestSubstring.java
@@ -4,26 +4,32 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LengthOfLongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
-        Set<Character>set=new HashSet<>();
-        int maxLength=0;
-        int left=0;
-        for(int right=0;right<s.length();right++){
-           
-            if(!set.contains(s.charAt(right))){
-                set.add(s.charAt(right));
-                maxLength=Math.max(maxLength,right-left+1);
-                
-            }else{
-                while(s.charAt(left)!=s.charAt(right)){
-                    set.remove(s.charAt(left));
-                    left++;
-                }
-                set.remove(s.charAt(left));left++;
+    static int lengthOfLongestSubstring(String s) {
+        //without repeating characters
+        int left =0;
+        int maxLength = 0;
+         Set<Character> set = new HashSet();
+         for(int right =0; right < s.length(); right++)
+         {
+            if(!set.contains(s.charAt(right)))
+            {
                 set.add(s.charAt(right));
             }
-            
-        }
-        return maxLength;
+            else{
+                while(set.contains(s.charAt(right)))
+                {
+                    set.remove(s.charAt(left));
+                    left +=1;
+                }
+                set.add(s.charAt(right));
+            }
+            maxLength=Math.max(maxLength,right-left+1);
+
+         }
+         return maxLength;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(lengthOfLongestSubstring("dvfv"));
     }
 }
