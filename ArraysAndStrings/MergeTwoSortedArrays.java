c2aefd88aadca274c84c7a46c2256a08c72a0541
Deepika Teegapuram <dteegapuram@gmail.com>
2024-07-30 21:15:41 -0600

    Updated the logic for sorted arrays.

--- ArraysAndStrings/MergeTwoSortedArrays.java
+++ ArraysAndStrings/MergeTwoSortedArrays.java
@@ -2,39 +2,29 @@ package ArraysAndStrings;
 
 public class MergeTwoSortedArrays {
     static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index = 0;
-        int numsindex = 0;
-        int finalArray[] = new int[nums1.length];
-        for(int i=0; i< nums1.length; i++)
-        {
-           if(numsindex == m && index < n)
-           {
-              System.out.println("inside if: numsIndex: " + numsindex + " m: " +m+ " index: " + index+" n: " +n);
-              finalArray[i] = nums2[index];
-              index++;
-           }
-           if(index == n && numsindex < m)
-           {
-              System.out.println("inside if else: numsIndex: " + numsindex + " m: " +m+ " index: " + index+" n: " +n);
-              finalArray[i] = nums1[numsindex];
-              numsindex++;
-           }
-           if(nums1[numsindex] > nums2[index])
-           {
-              finalArray[i] = nums2[index];
-              index++;
-           }
-           else if(nums1[numsindex] <= nums2[index])
-           {
-              finalArray[i] = nums1[numsindex];
-              numsindex++;
-           }
-        }
-        nums1 = finalArray;
-        for (int i : finalArray) {
-            System.out.println(i);
-        }
-       // System.out.println(nums1);
+  //variables to work as pointers
+  int i=m-1; // will point at m-1 index of nums1 array
+  int j=n-1; // will point at n-1 index of nums2 array
+  int k=nums1.length-1; //will point at the last position of the nums1 array
+
+  // Now traversing the nums2 array
+  while(j>=0){
+      // If element at i index of nums1 > element at j index of nums2
+      // then it is largest among two arrays and will be stored at k position of nums1
+      // using i>=0 to make sure we have elements to compare in nums1 array
+      if(i>=0 && nums1[i]>nums2[j]){
+          nums1[k]=nums1[i];
+          k--; 
+          i--; //updating pointer for further comparisons
+      }else{
+          // element at j index of nums2 array is greater than the element at i index of nums1 array 
+          // or there is no element left to compare with the nums1 array 
+          // and we just have to push the elements of nums2 array in the nums1 array.
+          nums1[k] = nums2[j];
+          k--; 
+          j--; //updating pointer for further comparisons
+      }
+  }
     }
     public static void main(String args[])
     {
