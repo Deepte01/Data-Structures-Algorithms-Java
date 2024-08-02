f8e47aaec6c6496bff4f9cafa5c3581e13b3d500
Deepika Teegapuram <dteegapuram@gmail.com>
2024-08-01 20:58:59 -0600

    remove the duplcate from the sorted array

---
+++ ArraysAndStrings/RemoveDuplicates.java
@@ -0,0 +1,56 @@
+package ArraysAndStrings;
+
+import java.util.HashMap;
+
+public class RemoveDuplicates {
+    /*
+     * Example 1:
+        Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place 
+        such that each unique element appears only once. 
+        The relative order of the elements should be kept the same.
+        Then return the number of unique elements in nums.
+
+
+        Input: nums = [1,1,2]
+        Output: 2, nums = [1,2,_]
+        Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+        It does not matter what you leave beyond the returned k (hence they are underscores).
+        Example 2:
+
+        Input: nums = [0,0,1,1,1,2,2,3,3,4]
+        Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+        Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+        It does not matter what you leave beyond the returned k (hence they are underscores).
+
+     */
+    static int removeDuplicates(int[] nums) {
+        // I have used a two pointer approach.
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int i=1;
+        for(int k : nums)
+        {
+            map.put(k, map.getOrDefault(k, 0)+ 1);
+        }
+        for(int j = 0; j< nums.length;)
+        {
+            if(j > 0)
+            {
+              int nextIndex = map.get(nums[j]);
+              int temp = nums[i];
+              nums[i] = nums[j];
+              nums[j] = temp;
+              j = j+nextIndex;
+              i++;
+            }
+            else{
+                int nextIndex = map.get(nums[j]);
+                j = j+ nextIndex;
+            }
+        }
+        return i;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));  
+    }
+}
