93c340be717cf08ec42068e4a9db71b0e1f9f300
Deepika Teegapuram <dteegapuram@gmail.com>
2024-08-01 18:57:30 -0600

    update the logic for soft elemninating elements from array

--- ArraysAndStrings/RemoveElement.java
+++ ArraysAndStrings/RemoveElement.java
@@ -1,17 +1,25 @@
 package ArraysAndStrings;
 
 public class RemoveElement {
-    public int removeElement(int[] nums, int val) {
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-            }
+    static int removeElement(int[] nums, int val) 
+    {
+      int i = 0;
+      for(int j = 0; j < nums.length; j++)
+      {
+        if (nums[j] != val) {
+          int temp = nums[i];
+          nums[i] = nums[j];
+          nums[j] = temp;
+          i++;
         }
-        return i;
+      }
+      for (int k : nums) {
+        System.out.print(k);
+      }
+      return i;
     }
-
+    public static void main(String args[])
+    {
+        System.out.println(removeElement(new int[]{4,5}, 4));  
+    }
 }
