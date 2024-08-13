d9aa9aa5f8ae1f69fa16c58debcde4a8676b3693
Deepika Teegapuram <dteegapuram@gmail.com>
2024-08-12 19:34:47 -0600

    added a skeleton code to convert roman to integer problem.

---
+++ ArraysAndStrings/RomanToInteger.java
@@ -0,0 +1,29 @@
+package ArraysAndStrings;
+
+public class RomanToInteger {
+    /*
+     * Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+     */
+    static int romanToInt(String s) {
+        
+    }
+    public static void main(String args[])
+    {
+        System.out.println(romanToInt("III"));  
+    }
+}
