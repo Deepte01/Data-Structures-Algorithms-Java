d74c96e89ea4dcfba9205fb03b3b89b0422fd0d6
Deepika Teegapuram <dteegapuram@gmail.com>
2024-08-12 20:13:06 -0600

    added a new solution

--- ArraysAndStrings/RomanToInteger.java
+++ ArraysAndStrings/RomanToInteger.java
@@ -1,5 +1,8 @@
 package ArraysAndStrings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger {
     /*
      * Example 1:
@@ -35,6 +38,31 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
             else ans += num;
         }
         return ans;
+    }
+    static int romanToIntSol2(String s)
+    {
+        Map<Character, Integer> m = new HashMap<>();
+        
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        
+        int ans = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+        
+        return ans;
+
     }
     public static void main(String args[])
     {
