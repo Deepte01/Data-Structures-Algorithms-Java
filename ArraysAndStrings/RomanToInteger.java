4ec9e565e4e7b80d5509e5a6fc7d243535c6d638
Deepika Teegapuram <dteegapuram@gmail.com>
2024-08-12 19:35:45 -0600

    added a switch case

--- ArraysAndStrings/RomanToInteger.java
+++ ArraysAndStrings/RomanToInteger.java
@@ -20,7 +20,19 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
      */
     static int romanToInt(String s) {
-        
+        int ans = 0, num = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            switch(s.charAt(i)) {
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+        }
+        return ans;
     }
     public static void main(String args[])
     {
